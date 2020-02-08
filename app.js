a54a371026099d44d5c7dd862bb63dc93d3772ec
ing-arriola <ingeniero.arriola@gmail.com>
2020-02-08 12:48:10 -0600

    Added once directive

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 new Vue({
     el:'#app',
     data:{
-        title:'Hello World!!'
+        greeting:'Hello World!!'
     },
     methods:{
         sayHello:function(){
-            this.title='Hello App'
-            return this.title
+            this.greeting='Hello App'
+            return this.greeting
         }
     }
 
